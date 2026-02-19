5e6fe232cb30c2370659af1fa9c54f7ec0538b40
Shailendra Kumar <shailendrauno89@gmail.com>
2026-02-19 14:31:48 +0530

    the signup is running

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,8 @@
 import Link from "next/link"
 import React from "react"
 import { useRouter } from "next/navigation"
-import {axios} from "axios"
+import axios from "axios"
+import LoginPage from "../login/page"
 
 export default function SignupPage(){
     const [user, setUser] = React.useState({
@@ -18,14 +19,44 @@ export default function SignupPage(){
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Signup page</h1>
-            <hr />
+            <hr /> <hr /> 
             <label htmlFor="username">username</label>
             <input 
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
             id="username"
             type="text" 
             value={user.username}
             onChange={(e)=> setUser({...user, username: e.target.value})}
+            placeholder="username"
             />
+            
+            <hr />
+            <label htmlFor="email">email</label>
+            <input 
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+            id="email"
+            type="text" 
+            value={user.email}
+            onChange={(e)=> setUser({...user, email: e.target.value})}
+            placeholder="email"
+            />
+
+            <hr />
+            <label htmlFor="password">password</label>
+            <input 
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+            id="password"
+            type="password" 
+            value={user.password}
+            onChange={(e)=> setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+
+            <button
+            onClick={onSignup}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup here</button>
+
+            <Link href="/login">Visit login page</Link>
         </div>
     )
 }
